5d6bc1879de99c2e4286877e6d754fdabd531682
wilsonturpo <wilson.turpo@tecsup.edu.pe>
2021-05-18 17:53:19 -0700

    Aplicación finalizada, lista con titulo, imagen y categoría elegida desde combo box

--- ColeccionDeJuegos/JuegosViewController.swift
+++ ColeccionDeJuegos/JuegosViewController.swift
@@ -10,7 +10,30 @@ import UIKit
 
 class JuegosViewController: UIViewController,
     UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate{
+    UINavigationControllerDelegate,
+    UITableViewDelegate,
+    UITableViewDataSource{
+    
+    @IBOutlet weak var Tabla: UITableView!
+    @IBOutlet weak var txtCategoria: UITextField!
+    
+    
+    @IBAction func addCategory(_ sender: Any) {
+        Tabla.isHidden = false
+    }
+    
+    
+    var arregloCategorias = ["Costa", "Sierra", "Selva"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arregloCategorias.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = arregloCategorias[indexPath.row]
+        return cell
+    }
     
     
     @IBAction func fotosTapped(_ sender: Any) {
@@ -22,21 +45,51 @@ class JuegosViewController: UIViewController,
     }
     
     @IBAction func agregarTapped(_ sender: Any) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
-        let juego = Juego(context:context)
-        juego.titulo = tituloTextField.text
-        juego.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
+        if(txtCategoria.text! == ""){
+            let alert = UIAlertController(title: "Debe seleccionar una categoria",
+                          message: "Presione en botón '+' que està abajo", preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            
+        }else{
+        
+            if juego != nil{
+                juego!.titulo! = tituloTextField.text!
+                juego!.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
+                juego!.categoria = txtCategoria.text!
+            }else{
+                let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                   
+                let juego = Juego(context:context)
+                juego.titulo = tituloTextField.text
+                juego.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
+                juego.categoria = txtCategoria.text!
+            }
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    
+    @IBAction func eliminarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(juego!)
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController?.popViewController(animated: true)
     }
     
     
-    
     @IBOutlet weak var JuegoImageView: UIImageView!
     @IBOutlet weak var tituloTextField: UITextField!
+    @IBOutlet weak var agregarActualizarBoton: UIButton!
+    @IBOutlet weak var eliminarBoton: UIButton!
+    
     
     var imagePicker = UIImagePickerController()
+    var juego:Juego? = nil
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let imagenSeleccionada = info[.originalImage] as? UIImage
@@ -46,9 +99,27 @@ class JuegosViewController: UIViewController,
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Tabla.dataSource = self
+        Tabla.delegate = self
+        Tabla.isHidden = true
+        
         imagePicker.delegate = self
+        
+        if juego != nil{
+            JuegoImageView.image = UIImage(data: (juego!.imagen!) as Data)
+            tituloTextField.text = juego!.titulo
+            txtCategoria.text = juego!.categoria
+            agregarActualizarBoton.setTitle("Actualizar", for: .normal)
+        }else{
+            eliminarBoton.isHidden = true
+        }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let categoria = arregloCategorias[indexPath.row]
+        txtCategoria.text = categoria
+        Tabla.isHidden = true
+    }
 
     /*
     // MARK: - Navigation
--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -3,18 +3,27 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate,
 UITableViewDataSource{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.tableView.isEditing = true
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return juegos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Celda", for: indexPath)
         let juego = juegos[indexPath.row]
         cell.textLabel?.text = juego.titulo
         cell.imageView?.image = UIImage(data: (juego.imagen!))
+        cell.detailTextLabel?.text = juego.categoria
         return cell
     }
-    
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -31,13 +40,36 @@ UITableViewDataSource{
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
-        
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
+    }
+    
+    //Habilitando la función de editar
+    func tableView(_ tableView: UITableView, commit editingStyle:UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == .delete{
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            print(juegos.count)
+            print(juegos[indexPath.row])
+            context.delete(juegos[indexPath.row])
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            navigationController?.popViewController(animated: true)
+            juegos.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    //Habilitando la reordenación de elementos
+    func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let juegoMovido = self.juegos[fromIndexPath.row]
+        juegos.remove(at: fromIndexPath.row)
+        juegos.insert(juegoMovido, at: to.row)
+    }
+    
 }
 
